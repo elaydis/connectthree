27d81627313cc2110b89938cfa3b423369f9e68c
Nadine Kost <nadine@door2door.io>
2017-04-27 11:54:44 +0200

    New maps api key

--- app/src/main/java/io/door2door/connectthree/PlannerActivity.java
+++ app/src/main/java/io/door2door/connectthree/PlannerActivity.java
@@ -1,8 +1,7 @@
 package io.door2door.connectthree;
 
-import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-
+import android.support.v4.app.FragmentActivity;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -19,7 +18,8 @@ public class PlannerActivity extends FragmentActivity implements OnMapReadyCallb
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_planner);
     // Obtain the SupportMapFragment and get notified when the map is ready to be used.
-    SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+    SupportMapFragment mapFragment =
+        (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
     mapFragment.getMapAsync(this);
   }
 
