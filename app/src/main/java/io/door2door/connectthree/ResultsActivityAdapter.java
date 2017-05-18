b6b036229d4863d431d11fa736f63541a5b90d36
Dylan Maryk <dylan@door2door.io>
2017-05-18 15:31:47 +0200

    Change information. Make cards taller.

--- app/src/main/java/io/door2door/connectthree/ResultsActivityAdapter.java
+++ app/src/main/java/io/door2door/connectthree/ResultsActivityAdapter.java
@@ -15,22 +15,27 @@ import java.util.Arrays;
 
 class ResultsActivityAdapter extends RecyclerView.Adapter<ResultsActivityAdapter.ViewHolder> {
   private ArrayList<String> transportTypes = new ArrayList<>(
-      Arrays.asList("Ride-Share", "Public Transport", "Public Transport", "Public Transport"));
-  private ArrayList<String> durations = new ArrayList<>(Arrays.asList("15", "28", "31", "25"));
+      Arrays.asList("Ride-Share", "Ride-Share", "Public Transport", "Public Transport",
+          "Public Transport"));
+  private ArrayList<String> remainingTimes = new ArrayList<>(
+      Arrays.asList("ETA 8 minutes", "ETA 5 minutes", "leaves in 5 minutes", "leaves in 5 minutes",
+          "leaves in 5 minutes"));
+  private ArrayList<String> durations =
+      new ArrayList<>(Arrays.asList("35", "25", "15", "20", "25"));
   private ArrayList<String> prices =
-      new ArrayList<>(Arrays.asList("1,00 €", "2,80 €", "2,80 €", "2,80 €"));
+      new ArrayList<>(Arrays.asList("1,20 €", "2,30 €", "2,80 €", "2,80 €", "2,80 €"));
   private ArrayList<String> starts = new ArrayList<>(
       Arrays.asList("Rosenthaler Platz", "Rosenthaler Platz", "Rosenthaler Platz",
-          "Rosa-Luxemburg  Platz"));
+          "Rosenthaler Platz", "Rosa-Luxemburg  Platz"));
   private ArrayList<String> ends = new ArrayList<>(
-      Arrays.asList("Potsdamer Platz", "S+U Potsdamer Platz", "Berlin Potsdamer Platz Bahnhof",
-          "S+U Potsdamer Platz"));
+      Arrays.asList("Potsdamer Platz", "Potsdamer Platz", "S+U Potsdamer Platz",
+          "Berlin Potsdamer Platz Bahnhof", "S+U Potsdamer Platz"));
   private ArrayList<Integer> summaryImages = new ArrayList<>(
       Arrays.asList(R.drawable.route_summary_1, R.drawable.route_summary_1,
-          R.drawable.route_summary_2, R.drawable.route_summary_3));
+          R.drawable.route_summary_1, R.drawable.route_summary_2, R.drawable.route_summary_3));
   private ArrayList<Integer> detailImages = new ArrayList<>(
       Arrays.asList(R.drawable.route_details_1, R.drawable.route_details_1,
-          R.drawable.route_details_2, R.drawable.route_details_3));
+          R.drawable.route_details_1, R.drawable.route_details_2, R.drawable.route_details_3));
   private final BookButtonClickListener bookButtonClickListener;
 
   ResultsActivityAdapter(BookButtonClickListener bookButtonClickListener) {
@@ -47,7 +52,7 @@ class ResultsActivityAdapter extends RecyclerView.Adapter<ResultsActivityAdapter
 
   @Override
   public void onBindViewHolder(ViewHolder holder, int position) {
-    if (position == 0) {
+    if (position == 0 || position == 1) {
       holder.rideSharingBookingLayout.setVisibility(View.VISIBLE);
       holder.publicTransportSummaryImageView.setVisibility(View.GONE);
       holder.rideSharingPriceLayout.setVisibility(View.VISIBLE);
@@ -60,11 +65,15 @@ class ResultsActivityAdapter extends RecyclerView.Adapter<ResultsActivityAdapter
     }
     holder.transportTypeTextView.setText(
         transportTypes.toArray(new String[transportTypes.size()])[position]);
+    holder.remainingTimeTextView.setText(
+        remainingTimes.toArray(new String[remainingTimes.size()])[position]);
     holder.travelDurationTextView.setText(
         durations.toArray(new String[durations.size()])[position]);
     holder.priceTextView.setText(prices.toArray(new String[prices.size()])[position]);
     holder.startTextView.setText(starts.toArray(new String[starts.size()])[position]);
     holder.endTextView.setText(ends.toArray(new String[ends.size()])[position]);
+    holder.publicTransportPriceTextView.setText(
+        prices.toArray(new String[prices.size()])[position]);
     holder.publicTransportSummaryImageView.setImageResource(
         summaryImages.toArray(new Integer[summaryImages.size()])[position]);
     holder.publicTransportDetailsImageView.setImageResource(
@@ -73,13 +82,15 @@ class ResultsActivityAdapter extends RecyclerView.Adapter<ResultsActivityAdapter
 
   @Override
   public int getItemCount() {
-    return 4;
+    return 5;
   }
 
   class ViewHolder extends RecyclerView.ViewHolder {
 
     @BindView(R.id.transportTypeTextView)
     TextView transportTypeTextView;
+    @BindView(R.id.remainingTimeTextView)
+    TextView remainingTimeTextView;
     @BindView(R.id.travelDurationTextView)
     TextView travelDurationTextView;
     @BindView(R.id.priceTextView)
@@ -88,6 +99,8 @@ class ResultsActivityAdapter extends RecyclerView.Adapter<ResultsActivityAdapter
     TextView startTextView;
     @BindView(R.id.endTextView)
     TextView endTextView;
+    @BindView(R.id.publicTransportPriceTextView)
+    TextView publicTransportPriceTextView;
     @BindView(R.id.publicTransportSummaryImageView)
     ImageView publicTransportSummaryImageView;
     @BindView(R.id.publicTransportDetailsImageView)
